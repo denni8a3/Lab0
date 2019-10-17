f67368c1db2ce2d17e78f6b970eb89cb90c052fd
denni8a3 <37115861+denni8a3@users.noreply.github.com>
2019-10-17 08:22:09 -0400

    Update index.js

--- lab3/src/components/Card/index.js
+++ lab3/src/components/Card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-class Card extends React.components {
+class Card extends React.Component {
   render() {
     return (<div className ="card">{this.props.content}</div>);
   }
