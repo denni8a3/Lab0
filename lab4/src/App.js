c8ccbdd194661289f6d50350f97347a024fd62fd
denni8a3 <37115861+denni8a3@users.noreply.github.com>
2019-10-18 18:00:27 -0400

    cards ready
    listed

---
+++ lab4/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardList from "./components/CardList/index.js";
+import './App.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <CardList />
+      </div>
+    );
+  }
+}
+
+export default App;
